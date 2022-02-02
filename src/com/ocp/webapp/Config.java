7baf07056ce46a4aa7f5e2452b73471578a6d553
Ivan Soldatov <38758452+ivansoldatov@users.noreply.github.com>
2022-02-02 12:20:54 +0300

    L16

--- src/com/ocp/webapp/Config.java
+++ src/com/ocp/webapp/Config.java
@@ -11,7 +11,18 @@ import java.util.Properties;
 
 public class Config {
 
-    private static final File PROPS = new File("C:/java/projects/basejava/config/resume.properties");
+//    private static final File PROPS = new File(getHomeDir(),"C:/java/projects/basejava/config/resume.properties");
+private static final File PROPS = new File(getHomeDir(),"config/resume.properties");
+
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
+    }
+
     private static final Config INSTANCE = new Config();
     private final File storageDir;
     private final Storage storage;
